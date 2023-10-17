1fb93c300ba57c9c706dfd6c5287e8db282ede93
subhomoy samanta <subhomoy@keylines.net>
2023-10-17 11:09:46 +0530

    student feedback page create

--- resources/views/front/dashboard/elements/sidebar.blade.php
+++ resources/views/front/dashboard/elements/sidebar.blade.php
@@ -9,12 +9,14 @@
         <?php if(session()->get('role') == 1){?>
             <li class="nav-item"><a class="nav-link" href="<?=url('/user/student-bookings')?>"><i class="fa fa-list" style="margin-right:10px;"></i> Bookings</a></li>
             <li class="nav-item"><a class="nav-link" href="<?=url('/user/student-transactions')?>"><i class="fa fa-inr" style="margin-right:10px;"></i> Transactions</a></li>
+            <li class="nav-item"><a class="nav-link" href="<?=url('/user/student-feedback-list')?>"><i class="fa fa-comment" style="margin-right:10px;"></i> Feedbacks</a></li>
         <?php  } ?>
         <?php if(session()->get('role') == 2){?>
             <li class="nav-item"><a class="nav-link" href="<?=url('/dashboard/mentor-availability')?>"><i class="fa fa-home" style="margin-right:10px;"></i> Mentor Availability</a></li>
             <li class="nav-item"><a class="nav-link" href="<?=url('/dashboard/mentor-services')?>"><i class="fa fa-home" style="margin-right:10px;"></i> Mentor Service</a></li>
             <li class="nav-item"><a class="nav-link" href="<?=url('/user/mentor-bookings')?>"><i class="fa fa-list" style="margin-right:10px;"></i> Bookings</a></li>
             <li class="nav-item"><a class="nav-link" href="<?=url('/user/mentor-transactions')?>"><i class="fa fa-inr" style="margin-right:10px;"></i> Transactions</a></li>
+            <li class="nav-item"><a class="nav-link" href="<?=url('/user/mentor-feedback-list')?>"><i class="fa fa-comment" style="margin-right:10px;"></i> Feedbacks</a></li>
         <?php  } ?>
     </ul>
 </div>
