ff270460892ddffe05a5440d4cf0a5af4d3ef933
subhomoy samanta <subhomoy@keylines.net>
2024-01-17 10:29:27 +0530

    stumento to mentrovert text change & footer captcha submit button hide

--- resources/views/front/layout-after-login.blade.php
+++ resources/views/front/layout-after-login.blade.php
@@ -188,7 +188,6 @@ $pageName = $routeName->uri();
           </script>
       <?php }?>
         <form action="#" method="POST" id="demo-form">
-            <br>
             <button class="g-recaptcha" data-sitekey="6LfEu0opAAAAAIP82Q9XnG0dYN81-_DteAszQFMN" data-callback="onSubmit" style="display: none;">Submit</button>
         </form>
    </body>
