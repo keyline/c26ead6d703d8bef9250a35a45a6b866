0cf7d4e7f599848661c6bab7b644c3cccc9ce132
subhomoy samanta <subhomoy@keylines.net>
2024-01-26 18:06:34 +0530

    admin panel mentor panel

--- resources/views/admin/maincontents/mentor/list.blade - Copy.php
+++ resources/views/admin/maincontents/mentor/list.blade - Copy.php
@@ -51,7 +51,7 @@ $controllerRoute = $module['controller_route'];
                 <tbody>
                   <?php if($rows){ $sl=1; foreach($rows as $row){?>
                     <tr class="tableaction_border">
-                      <th scope="row"><?=$sl++?></th>
+                      <th rowspan="2" scope="row"><?=$sl++?></th>
                       <td>
                         <?=$row->first_name.' '.$row->last_name?><br>
                         <?php if($row->team_meeting_link){?>
@@ -60,6 +60,13 @@ $controllerRoute = $module['controller_route'];
                       </td>
                       <td><?=$row->email?></td>
                       <td><?=$row->mobile?></td>
+                      <!-- <td>
+                        <?php if($row->profile_pic != ''){?>
+                          <img src="<?=$row->profile_pic?>" class="img-thumbnail" alt="<?=$row->first_name.' '.$row->last_name?>" style="width: 150px; height: 150px; margin-top: 10px;">
+                        <?php } else {?>
+                          <img src="<?=env('NO_IMAGE')?>" alt="<?=$row->first_name.' '.$row->last_name?>" class="img-thumbnail" style="width: 150px; height: 150px; margin-top: 10px;">
+                        <?php }?>
+                      </td> -->
                       <td><a href="<?=$row->social_url?>" target="_blank" class="badge bg-primary">Social Link</a></td>
                       <td><?=date_format(date_create($row->created_at), "M d, Y h:i A")?></td>
                       <td>
@@ -75,17 +82,20 @@ $controllerRoute = $module['controller_route'];
                         </h6>
                         <a target="_blank" href="<?=url('admin/' . $controllerRoute . '/payouts/'.Helper::encoded($row->user_id))?>" class="badge bg-info" title="Edit <?=$module['title']?>"><i class="fa fa-inr"></i> View Payouts</a>
                       </td>
+
                     </tr>
                     <tr class="tableaction_dark">
                       <td colspan="7">
                         <div class="tableaction_allbtn">
+                          <!-- <a href="<?=url('admin/' . $controllerRoute . '/edit/'.Helper::encoded($row->user_id))?>" class="btn btn-outline-primary btn-sm" title="Edit <?=$module['title']?>"><i class="fa fa-edit"></i></a> -->
                           <?php if($row->valid){?>
                             <a href="<?=url('admin/' . $controllerRoute . '/change-status/'.Helper::encoded($row->user_id))?>" class="btn btn-success " title="Activate <?=$module['title']?>"><i class="fa fa-check"></i> Click To Disapprove</a>
                           <?php } else {?>
                             <a href="<?=url('admin/' . $controllerRoute . '/change-status/'.Helper::encoded($row->user_id))?>" class="btn btn-danger " title="Deactivate <?=$module['title']?>"><i class="fa fa-times"></i> Click To Approve</a>
                             <a href="<?=url('admin/' . $controllerRoute . '/delete/'.Helper::encoded($row->user_id))?>" class="btn btn-outline-danger btn-sm" title="Delete <?=$module['title']?>" onclick="return confirm('Do You Want To Delete This <?=$module['title']?>');"><i class="fa fa-trash"></i> Delete</a>
                           <?php }?>
-                          
+                          <!-- <br><br>
+                           -->
                           <a target="_blank" href="<?=url('admin/' . $controllerRoute . '/profile/'.Helper::encoded($row->user_id))?>" class="btn badge btn-secondary" title="Profile of <?=$module['title']?>"><i class="fa fa-user"></i> Mentor Profile</a>
                           <a target="_blank" href="<?=url('admin/' . $controllerRoute . '/availability/'.Helper::encoded($row->user_id))?>" class="btn badge bg-success" title="Edit <?=$module['title']?>"><i class="fa fa-clock"></i> Availability</a>
                           <a target="_blank" href="<?=url('admin/' . $controllerRoute . '/assigned-services/'.Helper::encoded($row->user_id))?>" class="btn badge bg-primary" title="Edit <?=$module['title']?>"><i class="fa fa-wrench"></i> Assigned Services</a>
@@ -95,9 +105,11 @@ $controllerRoute = $module['controller_route'];
                           <?php if($row->is_featured){?>
                             <span class="badge bg-success">Marked As Featured</span>
                           <?php } else {?>
+                            <!-- <a href="<?=url('admin/' . $controllerRoute . '/change-status-featured/'.Helper::encoded($row->user_id))?>" class="btn btn-outline-danger btn-xs" title="Featured <?=$module['title']?>"><i class="fa fa-check"></i> Mark Featured</a> -->
                             <a href="<?=url('admin/' . $controllerRoute . '/change-status-featured/'.Helper::encoded($row->user_id))?>" class="btn btn-outline-danger btn-xs" title="Featured <?=$module['title']?>"><i class="fa fa-times"></i> Mark Featured</a>
                           <?php }?>
                         </div>
+
                       </td>
                     </tr>
                   <?php } }?>
