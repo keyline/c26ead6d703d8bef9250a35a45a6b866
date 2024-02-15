b76e892caecb2ecffaabc00bae9f3a71e2de1cba
subhomoy samanta <subhomoy@keylines.net>
2024-02-15 10:53:06 +0530

    client feedback points

--- resources/views/front/pages/mentors.blade.php
+++ resources/views/front/pages/mentors.blade.php
@@ -34,13 +34,13 @@ use App\Helpers\Helper;
                   <div class="input-group">
                      <select name="day_no" id="day_no">
                         <option value="" selected>Select Day</option>
-                        <option value="0">Sunday</option>
-                        <option value="1">Monday</option>
-                        <option value="2">Tuesday</option>
-                        <option value="3">Wednesday</option>
-                        <option value="4">Thursday</option>
-                        <option value="5">Friday</option>
-                        <option value="6">Saturday</option>
+                        <option value="1">Sunday</option>
+                        <option value="2">Monday</option>
+                        <option value="3">Tuesday</option>
+                        <option value="4">Wednesday</option>
+                        <option value="5">Thursday</option>
+                        <option value="6">Friday</option>
+                        <option value="7">Saturday</option>
                      </select>
                   </div>
                   <!-- <div class="input-group">
